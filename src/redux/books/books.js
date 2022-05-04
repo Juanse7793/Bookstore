9e071a82116057b09fa9ac4a5c4e5f7c89441a03
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-05-04 18:50:20 -0500

    get and add book actions edited in books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,10 +1,17 @@
 import { v4 as uuid } from 'uuid';
+import { fetchBooks, createBook } from './api';
 
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const GET_BOOK = 'bookstore/books/GET_BOOK';
 
-export default function booksReducer(state = [{ title: 'McGyver', author: 'tv', id: uuid() }], action) {
+export default function booksReducer(
+  state = [],
+  action,
+) {
   switch (action.type) {
+    case GET_BOOK:
+      return [...state, action.payload];
     case ADD_BOOK:
       return [...state, action.book];
     case REMOVE_BOOK:
@@ -14,12 +21,23 @@ export default function booksReducer(state = [{ title: 'McGyver', author: 'tv', 
   }
 }
 
-export function addBook(book) {
-  return {
+export const getBooks = () => async (dispatch) => {
+  const books = await fetchBooks();
+  dispatch({
+    type: GET_BOOK,
+    payload: books,
+  });
+  return books;
+};
+
+export const addBook = (book) => async (dispatch) => {
+  const newBook = await createBook({ ...book, item_id: uuid() });
+  dispatch({
     type: ADD_BOOK,
-    book,
-  };
-}
+    payload: newBook,
+  });
+  return true;
+};
 
 export function removeBook(id) {
   return {
